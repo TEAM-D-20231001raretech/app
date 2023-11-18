ded29a8559e63de409565fd954ca857592ce01bd
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2023-11-18 21:40:35 +0900

    DB.py修正

--- Chatapp/app.py
+++ Chatapp/app.py
@@ -1,36 +1,40 @@
-from flask import Flask,request, redirect, render_templete, session, flash, abort, url_for
+from flask import Flask, request, redirect, render_template, session, flash, abort, url_for,send_from_directory
 from datetime import timedelta
-import os
 import hashlib
 import uuid
 import re
-
+from werkzeug.utils import secure_filename
+import os, glob
 from models import dbConnect
 
-app = Flask(__name__)
-app.secret_key = uuid.uuod4().hex
-app.permanent_session_lifetime = timedelta(days=30)
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-#サインアップページの表示
+app = Flask(__name__)
+app.secret_key = uuid.uuid4().hex
+app.permanent_session_lifetime = timedelta(days=30)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# サインアップページの表示
 @app.route('/signup')
 def signup():
-    return render_templete('registration/signup.html')
+    return render_template('registration/signup.html')
 
 
-#サインアップ処理
-@app.route('/sign', methods=['POST'])
+# サインアップ処理
+@app.route('/signup', methods=['POST'])
 def userSignup():
     name = request.form.get('name')
     email = request.form.get('email')
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
 
-    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+&"
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
-    if name == '' or email == '' or password1 == '' or password2 == '':
+    if name == '' or email =='' or password1 == '' or password2 == '':
         flash('空のフォームがあるようです')
     elif password1 != password2:
-        flash('二つのパスワードの値が違います')
+        flash('二つのパスワードの値が違っています')
     elif re.match(pattern, email) is None:
         flash('正しいメールアドレスの形式ではありません')
     else:
@@ -39,8 +43,7 @@ def userSignup():
         DBuser = dbConnect.getUser(email)
 
         if DBuser != None:
-            flash('すでに登録されているようです')
-
+            flash('既に登録されているようです')
         else:
             dbConnect.createUser(uid, name, email, password)
             UserId = str(uid)
@@ -48,38 +51,41 @@ def userSignup():
             return redirect('/')
     return redirect('/signup')
 
-#ログインページの表示
-@app.route('/loguin')
-def login():
-    return render_templete('registration/login.html')
 
-#ログイン処理
+# ログインページの表示
+@app.route('/login')
+def login():
+    return render_template('registration/login.html')
+
+
+# ログイン処理
 @app.route('/login', methods=['POST'])
 def userLogin():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if email =='' or password =='':
-        flash ('空のフォームがあるようです')
+    if email =='' or password == '':
+        flash('空のフォームがあるようです')
     else:
         user = dbConnect.getUser(email)
         if user is None:
-            flash('このユーザは存在しません')
+            flash('このユーザーは存在しません')
         else:
             hashPassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
             if hashPassword != user["password"]:
-                flash('パスワードが間違っています')
+                flash('パスワードが間違っています！')
             else:
-                session['uid'] = user ["uid"]
+                session['uid'] = user["uid"]
                 return redirect('/')
-        return redirect('login')
+    return redirect('/login')
 
 
-#ログアウト
+# ログアウト
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect('/login')
+
 
 # チャンネル一覧ページの表示
 @app.route('/')
@@ -92,10 +98,28 @@ def index():
         channels.reverse()
     return render_template('index.html', channels=channels, uid=uid)
 
-#チャンネル一覧ページの表示
+
+# チャンネルの追加
 @app.route('/', methods=['POST'])
-def app_channel():
+def add_channel():
     uid = session.get('uid')
+    user_type = session.get('user_type')
+    if uid is None or user_type != 'admin':
+        return redirect('/login')
+    channel_name = request.form.get('channelTitle')
+    channel = dbConnect.getChannelByName(channel_name)
+    if channel == None:
+        channel_description = request.form.get('channelDescription')
+        dbConnect.addChannel(uid, channel_name, channel_description)
+        return redirect('/')
+    else:
+        error = '既に同じ名前のチャンネルが存在しています'
+        return render_template('error/error.html', error_message=error)
+
+
+# チャンネルの更新
+@app.route('/update_channel', methods=['POST'])
+def update_channel():
     uid = session.get("uid")
     if uid is None:
         return redirect('/login')
@@ -135,6 +159,7 @@ def detail(cid):
     cid = cid
     channel = dbConnect.getChannelById(cid)
     messages = dbConnect.getMessageAll(cid)
+    images = dbConnect.getImageAll(cid)
 
     return render_template('detail.html', messages=messages, channel=channel, uid=uid)
 
@@ -170,6 +195,38 @@ def delete_message():
 
     return redirect('/detail/{cid}'.format(cid = cid))
 
+# 画像をアップロード
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+app.add_url_rule(
+    "/uploads/<name>", endpoint="download_file", build_only=True
+)
+
 
 @app.errorhandler(404)
 def show_error404(error):
@@ -179,4 +236,3 @@ def show_error404(error):
 @app.errorhandler(500)
 def show_error500(error):
     return render_template('error/500.html'),500
-
--- Chatapp/util/DB.py
+++ Chatapp/util/DB.py
@@ -4,11 +4,12 @@ class DB:
     def getConnection():
         try:
             conn = pymysql.connect(
-            host = "db",
-            db = "chatapp",
-            user = "testuser",
-            charset = "utf8",
-            cursorclass = pymysql.cursors.DictCursor
+            host="db",
+            db="chatapp",
+            user="testuser",
+            password="testuser",
+            charset="utf8",
+            cursorclass=pymysql.cursors.DictCursor
         )
             return conn
         except (ConnectionError):
