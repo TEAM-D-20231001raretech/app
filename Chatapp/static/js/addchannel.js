b4581224a2d91afd5debde92a7b7519d4034fb2a
tsucchi_5482716 <128296030+tsucchi-5482716@users.noreply.github.com>
2023-11-23 19:32:23 +0900

    change index.html (#9)
    * change index.html
    
    * change file

--- Chatapp/static/js/addchannel.js
+++ Chatapp/static/js/addchannel.js
@@ -1,31 +1,71 @@
-function addChannel() {
-    var channelList = document.getElementById('channelList');
-    var channelNameInput = document.getElementById('channelName');
-    var channelName = channelNameInput.value.trim();
+document.addEventListener('DOMContentLoaded', function() {
+    const channelListElement = document.getElementById('channelList');
+    const CHANNEL_ICON_PATH = "../static/img/icon_pows.svg";
 
-    if (channelName) {
-      // 新しいチャンネル項目を作成
-        var newItem = document.createElement('div');
-        newItem.className = 'channel-item';
-        newItem.textContent = channelName;
+    // チャンネルリストを取得して表示する関数
+    function fetchChannels() {
+        fetch('/channels') // Flaskのエンドポイントを適宜修正してください
+            .then(response => response.json())
+            .then(data => {
+                channelListElement.innerHTML = '';
+                data.forEach(channel => {
+                    const channelItem = document.createElement('div');
+                    channelItem.classList.add("channel-item");
 
-        // 削除ボタンを作成
-        var deleteBtn = document.createElement('button');
-        deleteBtn.textContent = '削除';
-        deleteBtn.className = 'delete-btn';
-        deleteBtn.onclick = function() {
-            channelList.removeChild(newItem);
-        };
+                    // アイコンを表示するimg要素を作成
+                    const iconImg = document.createElement('img');
+                    iconImg.src = CHANNEL_ICON_PATH;
+                    iconImg.alt = 'Channel Icon';
+                    iconImg.classList.add('channel-icon');
 
-        // 項目に削除ボタンを追加
-        newItem.appendChild(deleteBtn);
+                    // チャンネル名を表示するa要素を作成
+                    const channelLink = document.createElement('a');
+                    channelLink.href = `detail.html?channelId=${channel.id}`;
+                    channelLink.textContent = channel.name;
+                    channelLink.classList.add('channel-name');
 
-        // チャンネルリストに項目を追加
-        channelList.appendChild(newItem);
+                    // 削除ボタンの作成
+                    const deleteButton = document.createElement('button');
+                    deleteButton.innerText = '削除';
+                    deleteButton.classList.add('delete-button');
+                    deleteButton.onclick = function() {
+                        if (confirm('このチャンネルを削除しますか？')) {
+                            fetch(`/delete/${channel.id}`, { method: 'POST' }) // Flaskのエンドポイントを適宜修正してください
+                                .then(() => {
+                                    channelItem.remove();
+                                })
+                                .catch(error => {
+                                    console.error('Error:', error);
+                                });
+                        }
+                    };
+                    // チャンネル項目にアイコン、リンク、削除ボタンを追加
+                    channelItem.appendChild(iconImg);
+                    channelItem.appendChild(channelLink);
+                    channelItem.appendChild(deleteButton);
 
-        // 入力フィールドをクリア
-        channelNameInput.value = '';
-    } else {
-        alert('チャンネル名を入力してください。');
+                    // 完成したチャンネル項目リストを追加
+                    channelListElement.appendChild(channelItem);
+                });
+            });
     }
-    }
+
+    // チャンネルリストを取得
+    fetchChannels();
+
+    // チャンネル追加のフォーム送信イベントを処理
+    document.getElementById('channel-form').onsubmit = function(event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+        fetch('/', { method: 'POST', body: formData })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('サーバーエラーが発生しました');
+                }
+                fetchChannels(); // リストを再取得して表示を更新
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
+});
