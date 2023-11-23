cda13552d4431255e38ddcff8ef9ad03329af0b1
Miyuki Katada <happy.mk39@gmail.com>
2023-11-23 14:54:54 +0900

    detail遷移終了

--- MySQL/init.sql
+++ MySQL/init.sql
@@ -30,5 +30,5 @@ CREATE TABLE messages (
 );
 
 INSERT INTO users(uid, user_name, email, password)VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','チワワLOVE','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
-INSERT INTO channels(id, uid, name, abstract)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','うちの子が一番！','テストさんの孤独な部屋です');
-INSERT INTO messages(id, uid, cid, message)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '今日はおめかししました')
+INSERT INTO channels(id, uid, name, abstract)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','うちの子が一番！','うちの子自慢をするチャンネルです');
+INSERT INTO messages(id, uid, cid, message)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '今日はおめかししてお出かけしました')
